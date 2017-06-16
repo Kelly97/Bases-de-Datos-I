52efa367398e4efc18a600c16830b89fc1242883
Kelly97 <Kelly.marie.aguilar@gmail.com>
2017-06-16 07:08:28 -0600

    Carrusel

--- Flipboard/js/index.js
+++ Flipboard/js/index.js
@@ -8,23 +8,21 @@ $(document).ready(function() {
 	      dots: false,
 		  infinite: false,
 		  speed: 300,
-		  slidesToShow: 7,
-		  slidesToScroll: 7,
+		  slidesToShow: 12,
+		  slidesToScroll: 11,
 		  responsive: [
 		    {
 		      breakpoint: 1024,
 		      settings: {
-		        slidesToShow: 3,
-		        slidesToScroll: 3,
-		        infinite: true,
-		        dots: true
+		        slidesToShow: 7,
+		        slidesToScroll: 6
 		      }
 		    },
 		    {
-		      breakpoint: 600,
+		      breakpoint: 1000,
 		      settings: {
-		        slidesToShow: 2,
-		        slidesToScroll: 2
+		        slidesToShow: 5,
+		        slidesToScroll: 5
 		      }
 		    },
 		    {
@@ -42,7 +40,7 @@ $(document).ready(function() {
 });
 
 function ajustarContenedorNoticias(){
-	var altoNavbar = $(navbar).height() + 15;
+	var altoNavbar = $("#navbar-sup").height() + 40;
 	$('#contenido-principal').css("margin-top",altoNavbar+"px");
 }
 
