a44b8043cea2f430bfea99f1394ed05ace8d75fb
Allan J. Martínez <Jafectnez@gmail.com>
2017-06-24 17:50:56 -0600

    Tabla de Comentarios 2do intento y retoques en la interfaz noticia
    pues eso

--- Flipboard/js/noticia.js
+++ Flipboard/js/noticia.js
@@ -1,3 +1,47 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+          dots: false,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 8,
+          slidesToScroll: 7,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 7,
+                slidesToScroll: 6
+              }
+            },
+            {
+              breakpoint: 1000,
+              settings: {
+                slidesToShow: 5,
+                slidesToScroll: 5
+              }
+            },
+            {
+              breakpoint: 900,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+      });
+    $('[data-toggle="popover"]').popover();
+});
+
+function ajustarContenedorNoticias(){
+    var altoNavbar = $(".iconos-derecha").height() + 40;
+    $('#contenido-principal').css("margin-top",altoNavbar+"px");
+    var anchoIntereses = $(window).width() - $(".iconos-derecha").width()-170;
+    $('.your-class').css("width",anchoIntereses+"px");
+}
+
 function cargarNoticias(codigo){
 	data = "codigo="+codigo;
 	$.ajax({ 
