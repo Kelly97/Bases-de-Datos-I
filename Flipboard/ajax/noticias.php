95a897c23593cd5bfd15ce50121dcd08369a87a6
Kelly97 <Kelly.marie.aguilar@gmail.com>
2017-06-25 08:29:13 -0600

    Plataforma

--- Flipboard/ajax/noticias.php
+++ Flipboard/ajax/noticias.php
@@ -4,9 +4,13 @@ switch ($_POST["codigo"]) {
 	case 0:
 		?>
 		<div class="row">
+		  <div class="col-lg-12 col-md-12" style="text-align: center;padding-bottom: 40px;">
+		     <h2>Noticias de Portada</h2>
+		     <h5 style="color: #999;">Noticias de gran relevancia en un solo lugar</h5>
+		  </div>
 		  <div class="col-lg-4 col-md-6">
 		    <div class="thumbnail" style="position: relative;">		    
-		      <div class="botones-noticia">
+		      <div class="botones-noticia-general">
 		      	<button type="button" class="btn btn-danger btn-circle" data-container="body" data-toggle="popover" data-placement="left" data-content="Flipear" data-trigger="hover">
 		      		<i class="fa fa-plus" aria-hidden="true"></i>
 		      	</button><br>
@@ -18,23 +22,25 @@ switch ($_POST["codigo"]) {
 		      	</button>
 		      </div>
 		      <a href="noticia.html">
-		      <img src="images/img_prueba.jpg" alt="...">
-		      <div class="caption">
-		        <h3>Titulo Noticia</h3>
-		        <h5>Autor o propietario de noticia</h5>
-		        <p style="text-align: justify;">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó en. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicostware de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
-		        <p>
-		        	5 
-		        	<i class="fa fa-heart" aria-hidden="true" style="font-size: 13px;padding-right: 8px;">
-			        </i>
-		        	<a class="btn btn-default" role="button">
-			        	<i class="fa fa-plus-circle" aria-hidden="true" style="font-size: 13px;padding-right: 8px;">
-			        		
-			        	</i>
-			        	Añadir Comentario
-		        	</a>
-		        </p>
-		      </div>
+			      <img src="images/img_prueba.jpg" alt="...">
+			      <div class="caption">
+			        <h3>Titulo Noticia</h3>
+			        <h5>Autor o propietario de noticia</h5>
+			        <p style="text-align: justify;">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó en. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicostware de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+			        <p>
+			        	5 
+			        	<i class="fa fa-heart" aria-hidden="true" style="font-size: 13px;padding-right: 8px;color: red;">
+				        </i>
+				        3
+				        <i class="fa fa-comment-o" aria-hidden="true" style="font-size: 13px;padding-right: 8px;"></i>
+			        	<a class="btn btn-default" role="button">
+				        	<i class="fa fa-plus-circle" aria-hidden="true" style="font-size: 13px;padding-right: 8px;">
+				        		
+				        	</i>
+				        	Añadir Comentario
+			        	</a>
+			        </p>
+			      </div>
 		      </a>
 		    </div>
 		  </div>	
@@ -44,25 +50,55 @@ switch ($_POST["codigo"]) {
 		<?php
 		break;
 
-	case 1:
+	default:
 		?>
-		<h3>Titulo Noticia</h3>
-	    <h5>Autor o propietario de noticia</h5>
-		<img src="images/img_prueba.jpg" alt="..."><br>
-	    <div class="caption">
-		    <p style="text-align: justify;">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó en. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicostware de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
-		    <p>
-		      	<a class="btn btn-default" role="button">
-		      		<i class="fa fa-plus-circle" aria-hidden="true" style="font-size: 13px;padding-right: 8px;"></i>
-		      		Añadir Comentario
-		        </a>
-	        </p>
-        </div>
+		<div class="row">
+		  <div class="col-lg-12 col-md-12" style="text-align: center;padding-bottom: 40px;">
+	        <h2>Interés</h2>
+	        <h5 style="color: #999;">Código: <?php echo $_POST["codigo"] ?></h5>
+	        <a class="btn btn-default btn-seguir" role="button">Seguir</a>
+	        <a class="btn btn-default btn-seguir" role="button" style="border:none;" data-container="body" data-toggle="popover" data-placement="left" data-content="Eliminar Interés" data-trigger="hover">
+	        	<i class="fa fa-times" aria-hidden="true"></i>
+	        </a>
+		  </div>
+		  <div class="col-lg-4 col-md-6">
+		    <div class="thumbnail" style="position: relative;">		    
+		      <div class="botones-noticia-general">
+		      	<button type="button" class="btn btn-danger btn-circle" data-container="body" data-toggle="popover" data-placement="left" data-content="Flipear" data-trigger="hover">
+		      		<i class="fa fa-plus" aria-hidden="true"></i>
+		      	</button><br>
+		      	<button type="button" class="btn btn-default btn-circle" data-container="body" data-toggle="popover" data-placement="left" data-content="Me gusta" data-trigger="hover">
+		      		<i class="fa fa-heart-o" aria-hidden="true"></i>
+		      	</button><br>
+		      	<button type="button" class="btn btn-default btn-circle" data-container="body" data-toggle="popover" data-placement="left" data-content="Compartir" data-trigger="hover">
+		      		<i class="fa fa-envelope-o" aria-hidden="true"></i>
+		      	</button>
+		      </div>
+		      <a href="noticia.html">
+			      <img src="images/img_prueba.jpg" alt="...">
+			      <div class="caption">
+			        <h3>Titulo Noticia</h3>
+			        <h5>Autor o propietario de noticia</h5>
+			        <p style="text-align: justify;">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó en. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicostware de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+			        <p>
+			        	5 
+			        	<i class="fa fa-heart" aria-hidden="true" style="font-size: 13px;padding-right: 8px;color: red;">
+				        </i>
+				        3
+				        <i class="fa fa-comment-o" aria-hidden="true" style="font-size: 13px;padding-right: 8px;"></i>
+			        	<a class="btn btn-default" role="button">
+				        	<i class="fa fa-plus-circle" aria-hidden="true" style="font-size: 13px;padding-right: 8px;">
+				        		
+				        	</i>
+				        	Añadir Comentario
+			        	</a>
+			        </p>
+			      </div>
+		      </a>
+		    </div>
+		  </div>		  	  
+		</div>
 		<?php
 		break;	
-	default:
-		echo "Adios";
-		# code...
-		break;
 }
 ?>
