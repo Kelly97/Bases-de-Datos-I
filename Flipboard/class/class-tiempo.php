5ab479913318ae77c0f41334b1db5e3db8e2ef07
NDz23 <diaznelson23@gmail.com>
2017-10-28 10:51:10 -0600

    Notificaciones

--- Flipboard/class/class-tiempo.php
+++ Flipboard/class/class-tiempo.php
@@ -10,14 +10,34 @@
 		}
 		public function calcularTiempoTranscurrido($minutos){
 			$this->setMinutos($minutos);
-			if ($this->minutos < 60)//menos de 1 hora
-				$this->tiempoTranscurrido = 'Hace '.(int)$this->minutos.' minutos.';
-			elseif ($this->minutos < 1440)//menos de 1 dia
-				$this->tiempoTranscurrido = 'Hace '.(int)($this->minutos/60).' horas.';
-			elseif ($this->minutos < 10080)//menos de 1 semana
-				$this->tiempoTranscurrido = 'Hace '.(int)($this->minutos/1440).' días.';
-			elseif ($this->minutos < 50040)//menos de 5 semanas
-				$this->tiempoTranscurrido = 'Hace '.(int)($this->minutos/10080).' semanas.';
+			if ($this->minutos < 60){//menos de 1 hora
+				$tiempoTemp = (int)$this->minutos;
+				if ($tiempoTemp == 1)
+					$this->tiempoTranscurrido = 'Hace '.$tiempoTemp.' minuto.';
+				else
+					$this->tiempoTranscurrido = 'Hace '.$tiempoTemp.' minutos.';
+			}
+			elseif ($this->minutos < 1440){//menos de 1 dia
+				$tiempoTemp = (int)($this->minutos/60);
+				if ($tiempoTemp == 1)
+					$this->tiempoTranscurrido = 'Hace '.$tiempoTemp.' hora.';
+				else
+					$this->tiempoTranscurrido = 'Hace '.$tiempoTemp.' horas.';
+			}
+			elseif ($this->minutos < 10080){//menos de 1 semana
+				$tiempoTemp = (int)($this->minutos/1440);
+				if ($tiempoTemp == 1)
+					$this->tiempoTranscurrido = 'Hace '.$tiempoTemp.' día.';
+				else
+					$this->tiempoTranscurrido = 'Hace '.$tiempoTemp.' días.';
+			}
+			elseif ($this->minutos < 50040){//menos de 5 semanas
+				$tiempoTemp = (int)($this->minutos/10080);
+				if ($tiempoTemp == 1)
+					$this->tiempoTranscurrido = 'Hace '.$tiempoTemp.' semana.';
+				else
+					$this->tiempoTranscurrido = 'Hace '.$tiempoTemp.' semanas.';
+			}
 			else
 				$this->tiempoTranscurrido = 'Hace mucho tiempo.';
 			return $this->tiempoTranscurrido;
