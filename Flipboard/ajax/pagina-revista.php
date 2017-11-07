de3dacc6fe6e84ae6774dfc7a2170b8164cd167e
NDz23 <diaznelson23@gmail.com>
2017-11-07 17:57:49 -0600

    Revista

--- Flipboard/ajax/pagina-revista.php
+++ Flipboard/ajax/pagina-revista.php
@@ -6,31 +6,116 @@ $codigoRevista = $_POST['codigoRevista'];//Obtenemos el código por medio de la 
 $conexion = new Conexion();
 $codigoUsuario = 1;//sesion
 $sql =  "
-	SELECT A.NOMBRE_REVISTA, A.DESCRIPCION, A.FECHA_DE_CREACION, A.URL_PORTADA, B.NOMBRE_USUARIO
+	SELECT UPPER(A.NOMBRE_REVISTA) NOMBRE_REVISTA, A.DESCRIPCION, A.FECHA_DE_CREACION, A.URL_PORTADA, B.NOMBRE_USUARIO, B.URL_FOTO_PERFIL, substr(B.NOMBRE_USUARIO,1,1) AS INICIAL, A.CODIGO_TIPO_REVISTA
 	FROM TBL_REVISTAS A
 	LEFT JOIN TBL_USUARIOS B
 	ON (A.CODIGO_USUARIO = B.CODIGO_USUARIO)
 	WHERE (A.CODIGO_REVISTA = $codigoRevista)
 	";
-$sql2 = "SELECT COUNT(*) AS NUMBER_OF_ROWS FROM ($sql)";
+$sql2 = "
+	SELECT B.NOMBRE_USUARIO, B.URL_FOTO_PERFIL,  substr(B.NOMBRE_USUARIO,1,1) AS INICIAL
+	FROM TBL_COLABORADORES A
+	LEFT JOIN TBL_USUARIOS B
+	ON (A.CODIGO_COLABORADOR = B.CODIGO_USUARIO)
+	WHERE (A.CODIGO_REVISTA = $codigoRevista)
+	";
+$sql3 = "SELECT COUNT(*) AS NUMBER_OF_ROWS FROM ($sql2)";
 $resultado = $conexion->ejecutarInstruccion($sql);
 $resultado2 = $conexion->ejecutarInstruccion($sql2);
-$cantidadRegistros = $conexion->obtenerArregloAsociativo($resultado2)['NUMBER_OF_ROWS'];
+$resultado3 = $conexion->ejecutarInstruccion($sql3);
+$datosRevista = $conexion->obtenerArregloAsociativo($resultado);
+$cantidadColaboradores = $conexion->obtenerArregloAsociativo($resultado3)['NUMBER_OF_ROWS'];
 ?>
-<div class="head-revista" style="margin: auto;background-image: url(../images/alternativo.jpg);width: 100%px;height: 90%;padding: 10px;"> 
-	<h2><strong>TITULO DE LA REVISTA</strong></h2>
-	<p>Descripcion de la revista</p>
-  	<button type="button" class="btn btn-default btn-semcircle" data-content="Seguir" data-trigger="hover">
-  		Seguir
-  	</button>
-  	<button type="button" class="btn btn-default btn-circle">
-  		<i class="fa fa-ellipsis-h" aria-hidden="true"></i>
-  	</button>
-  	<div>
-  		<button type="button" class="btn btn-default btn-circle-perfil">
-  			<i class="fa fa-user-plus fa-2x" aria-hidden="true"></i>
-  		</button>
-  	</div>
+<div class="head-revista" style="margin: auto; background-image: url('<?php echo $datosRevista['URL_PORTADA']; ?>');width: 100%;height: 90%;padding: 0px;">
+	<div class="head-revista" style="margin: auto; opacity:0.8;background-image: url();width: 100%;height: 100%;padding: 0px;"><!-- Este div tiene fondo negro con transparencia para asegurar que el texto e iconos dentro de el son siempre legible -->
+		<table style="height: 100%;width: 100%;font-size: 20px;font-weight: bold;">
+			<tbody>
+				<tr>
+					<td class="align-middle text-center">
+					<h2><strong><?php echo ($datosRevista['NOMBRE_REVISTA']); ?></strong></h2>
+						<?php
+						if(!is_null($datosRevista['DESCRIPCION'])){
+		        			?>
+							<p><?php echo ($datosRevista['DESCRIPCION']); ?></p>
+							<?php
+	            		}
+	            		?>	
+					  	<button type="button" class="btn btn-default btn-seguir-revista" data-content="Seguir" data-trigger="hover">
+					  		Seguir
+					  	</button>
+					  	<button type="button" class="btn btn-default btn-seguir-revista" data-content="Añadir/Quitar a/de intereses.">
+					  		<i class="fa fa-ellipsis-h" aria-hidden="true"></i>
+					  	</button>
+					</td>
+				</tr>
+				<tr>
+					<td>
+				  	<div class="row">
+				  		<!--Miniatura de la imagen-->
+	                	<div class="miniatura-usuario" style="margin: auto;background-image: url('<?php echo $datosRevista["URL_FOTO_PERFIL"]; ?>');width: 40px;height: 40px;padding: 0px;">
+	                		<?php
+	                		if(is_null($datosRevista["URL_FOTO_PERFIL"])){
+	                			?>
+	                				<table style="height: 100%;width: 100%;font-size: 20px;font-weight: bold;">
+										<tbody>
+											<tr>
+												<td class="align-middle text-center">
+													<?php echo ($datosRevista['INICIAL']); ?>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+	                			<?php
+	                		}
+	                		?>								
+					    </div>
+					    <!--FIN Miniatura de la imagen-->
+				  		<?php
+				  		$strAutores = $datosRevista['NOMBRE_USUARIO'];
+				  		if($cantidadColaboradores > 0){
+				  			for ($i=0; $i < $cantidadColaboradores; $i++) {
+				  				$colaborador = $conexion->obtenerArregloAsociativo($resultado2);
+				  				if ($cantidadColaboradores - $i > 1) {
+				  					$strAutores = $strAutores . ', ' . $colaborador['NOMBRE_USUARIO'];
+				  				}
+				  				else
+				  					$strAutores = $strAutores . ' y ' . $colaborador['NOMBRE_USUARIO'];
+				  				?>
+				  				<!--Miniatura de la imagen-->
+			                	<div class="miniatura-usuario" style="margin: auto;background-image: url('<?php echo $colaborador["URL_FOTO_PERFIL"]; ?>');width: 40px;height: 40px;padding: 0px;">
+			                		<?php
+			                		if(is_null($colaborador["URL_FOTO_PERFIL"])){
+			                			?>
+			                				<table style="height: 100%;width: 100%;font-size: 20px;font-weight: bold;">
+												<tbody>
+													<tr>
+														<td class="align-middle text-center">
+															<?php echo ($colaborador['INICIAL']); ?>
+														</td>
+													</tr>
+												</tbody>
+											</table>
+			                			<?php
+			                		}
+			                		?>								
+							  </div>
+							  <!--FIN Miniatura de la imagen-->
+							  <?php
+				  			}
+	            		}
+	            		?>	
+				  		<button type="button" class="btn btn-default btn-circle-revista" >
+				  			<i class="fa fa-user-plus fa-2x" aria-hidden="true"></i>
+				  		</button>
+				  	</div>
+				  	<div>
+				  		<t>Por <?php echo $strAutores; ?></t>
+				  	</div>
+				  </td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
 
 		<div class="col" style="text-align: center; padding: 10px">
@@ -42,7 +127,13 @@ $cantidadRegistros = $conexion->obtenerArregloAsociativo($resultado2)['NUMBER_OF
 		    # Noticias
 		  </div>
 		  <div class="col" style="text-align: center; padding: 10px">
-		    <i class="fa fa-lock" aria-hidden="true"></i><!--Se muestra si la revista es privada-->
+		  	<?php
+			if($datosRevista['CODIGO_TIPO_REVISTA'] == 2){
+    			?>
+				<i class="fa fa-lock" aria-hidden="true"></i>
+				<?php
+    		}
+    		?>	
 		    BAJA PARA VER MAS
 		    <i class="fa fa-chevron-down" aria-hidden="true"></i>
 		  </div>
--- Flipboard/js/perfil.js
+++ Flipboard/js/perfil.js
@@ -100,41 +100,13 @@ function cambiarCorreo(){
 }
 
 
-function cambiarFoto(){
-
-	$( "#files" ).click();
- 
-}
 
 
 
 
-function upload_image(){//Funcion encargada de enviar el archivo via AJAX
-				$(".upload-msg").text('Cargando...');
-				var inputFileImage = document.getElementById("files");
-				var file = inputFileImage.files[0];
-				var data = new FormData();
-				data.append('files',file);
-				
-				/*jQuery.each($('#fileToUpload')[0].files, function(i, file) {
-					data.append('file'+i, file);
-				});*/
-							
-				$.ajax({
-					url: "upload.php",        // Url to which the request is send
-					type: "POST",             // Type of request to be send, called as method
-					data: data, 			  // Data sent to server, a set of key/value pairs (i.e. form fields and values)
-					contentType: false,       // The content type used when sending data to the server.
-					cache: false,             // To unable request pages to be cached
-					processData:false,        // To send DOMDocument or non processed data file it is set to false
-					success: function(data)   // A function to be called if request succeeds
-					{
-						$(".upload-msg").html(data);
-						window.setTimeout(function() {
-						$(".alert-dismissible").fadeTo(500, 0).slideUp(500, function(){
-						$(this).remove();
-						});	}, 1000);
-					}
-				});
-				
-			}
+
+
+
+
+
+
